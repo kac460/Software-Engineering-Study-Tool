c3fb4e71f04c5956f729b34cb3dad12bb32b0642
kac460 <kac460@rutgers.edu>
2019-12-14 01:12:12 -0500

    Completed tool

--- study-tool.py
+++ study-tool.py
@@ -1,50 +1,72 @@
 import glob
 
+true_string_for_output = "TRUE"
+false_string_for_output = "FALSE"
+
 
 def first_is_false_or_true(seg):
-    true_string = "True"
-    false_string = "False"
     for j in range(len(seg)):
-        if seg[j:j + len(true_string)] == true_string:
-            return "True"
-        elif seg[j:j + len(false_string)] == false_string:
-            return "False"
+        if seg[j:j + len(true_string_for_output)] == "True":
+            return true_string_for_output
+        elif seg[j:j + len(false_string_for_output)] == "False":
+            return false_string_for_output
     return "Neither"
 
 
-html_files = []
+txt_files = []
 question_start_marker = 'textarea_question_text">&lt;p&gt;'
-question_end_marker = '&lt;'
-html_quote = '&nbsp;'
 question_answer_map = {}  # maps question (string) to corresponding answer (string)
-true_string_for_output = "TRUE"
-false_string_for_output = "FALSE"
-for file in glob.glob("*.html"):
-    html_files.append(file)
-for html_file in html_files:
-    f = open(html_file)
+
+for file in glob.glob("*.txt"):
+    txt_files.append(file)
+for text_file in txt_files:
+    if text_file == "answer key.txt" or text_file == "questions without answers.txt":
+        continue
+    f = open(text_file)
     s = f.read()
-    segments = s.split(question_start_marker)
-    for segment in segments:
-        question_raw = ""
-        for i in range(len(segment)):
-            if segment[i:i+len(question_end_marker)] == question_end_marker:
-                break
-            else:
-                question_raw += segment[i]
-        question_processed = question_raw.replace(html_quote, " ").strip()
-        segment_split_by_correct_answer_selection = segment.split("selected_answer correct_answer")
-        segment_split_by_incorrect_answer_selection = segment.split("wrong_answer")
-        if len(segment_split_by_correct_answer_selection) > 1:  # CORRECT ANSWER
-            if first_is_false_or_true(segment_split_by_correct_answer_selection[1]) == "True":
-                question_answer_map[question_processed] = true_string_for_output
-            else:
-                question_answer_map[question_processed] = false_string_for_output
-        elif len(segment_split_by_incorrect_answer_selection) > 1:  # INCORRECT ANSWER
-            if first_is_false_or_true(segment_split_by_incorrect_answer_selection[1]) == "True":
-                question_answer_map[question_processed] = false_string_for_output
-            else:
-                question_answer_map[question_processed] = true_string_for_output
+    if "Correct!" in s:
+        is_final_attempt = True
+    else:
+        is_final_attempt = False  # handled differently
+    question_split = s.split("Question")
+    if is_final_attempt:
+        for i in range(1, len(question_split)):
+            pts_split = question_split[i].split("pts")
+            pts_split_on_line_break = pts_split[1].split("\n")
+            # print(pts_split_on_line_break[1])
+            question = pts_split_on_line_break[1].strip()
+            if "0 / 0.4" in question_split[i]:  # INCORRECT RESPONSE
+                you_answered_split = question_split[i].split("Correct Answer")
+                correct_answer = first_is_false_or_true(you_answered_split[1])
+            else:  # CORRECT RESPONSE
+                split_on_correct_exclamation = question_split[i].split("Correct!")
+                correct_answer = first_is_false_or_true(split_on_correct_exclamation[1])
+            question_answer_map[question] = correct_answer
+    else:
+        for i in range(1, len(question_split)):
+            pts_split = question_split[i].split("pts")
+            pts_split_on_line_break = pts_split[1].split("\n")
+            question = pts_split_on_line_break[1].strip()
+            if "0 / 0.4" in question_split[i]:  # INCORRECT RESPONSE
+                you_answered_split = question_split[i].split("`")
+                if len(you_answered_split) <= 1:
+                    print("too small")
+                    continue
+                incorrect_answer = first_is_false_or_true(you_answered_split[1])
+                if incorrect_answer == false_string_for_output:
+                    print("set")
+                    question_answer_map[question] = true_string_for_output
+                else:
+                    print("set2")
+                    question_answer_map[question] = false_string_for_output
+            else:  # CORRECT RESPONSE
+                you_answered_split = question_split[i].split("`")
+                if len(you_answered_split) <= 1:
+                    continue
+                print("set3")
+                correct_answer = first_is_false_or_true(you_answered_split[1])
+                question_answer_map[question] = correct_answer
+
 
 answer_key_file = open("answer key.txt", 'w')
 questions_without_answers_file = open("questions without answers.txt", 'w')
